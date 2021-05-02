a6651ec3315d7ac7c827ca53c238e79a12d18fa1
laila-ba <75024926+laila-ba@users.noreply.github.com>
2021-05-02 12:47:28 +0100

    Add files via upload
    added file paths for images and attributes

---
+++ memory.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+//cards
+const cardArray = [
+  {
+    name:'black',
+    img:'./assets/img/black.edit.png'
+  },
+  {
+    name:'black',
+    img:'./assets/img/black.edit.png'
+  },
+  {
+    name:'blue',
+    img:'./assets/img/blue.edit.png'
+  },
+  {
+    name:'blue',
+    img:'./assets/img/blue.edit.png'
+  },
+  {
+    name:'green',
+    img:'./assets/img/green.edit.png'
+  },
+  {
+    name:'green',
+    img:'./assets/img/green.edit.png'
+  },
+  {
+    name:'orange',
+    img:'./assets/img/orange.edit.png'
+  },
+  {
+    name:'orange',
+    img:'./assets/img/orange.edit.png'
+  },
+  {
+    name:'purple',
+    img:'./assets/img/purple.edit.png'
+  },
+  {
+    name:'purple',
+    img:'./assets/img/purple.edit.png'
+  },
+  {
+    name:'yellow',
+    img:'./assets/img/yellow.edit.png'
+  },
+  {
+    name:'yellow',
+    img:'./assets/img/yellow.edit.png'
+  },
+]
+
+const game= document.querySelector('game')
+//game
+//loop over card array + create image elements
+  for (let i = 0; i < cardArray.length; i++){
+    var card = document.createElement('img')
+    card.setAttribute('src', 'img/blank.png')
+    card.setAttribute('data-id', i)
+    //card.addEventListener('click',flipcard)
+    game.appendChild(card)
+  }
+}
+createBoard()
