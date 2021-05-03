ec74548283e0a06f342d70185c7b4a6c56f0a1d0
laila-ba <75024926+laila-ba@users.noreply.github.com>
2021-05-03 14:18:31 +0100

    Add files via upload
    game code

--- memory.js
+++ memory.js
@@ -54,15 +54,61 @@ const cardArray = [
   },
 ]
 
+cardArray.sort(()=> 0.5 - Math.random())
+
 const game= document.querySelector('game')
+const resultDisplay= document.querySelector(''#result')
+var cardsChosen = []
+var cardsChosenId= []
+var cardsWon = []
 //game
 //loop over card array + create image elements
   for (let i = 0; i < cardArray.length; i++){
     var card = document.createElement('img')
     card.setAttribute('src', './assets/img/random.png')
     card.setAttribute('data-id', i)
-    //card.addEventListener('click',flipcard)
+    //card.addEventListener('click',flipCard)
     game.appendChild(card)
   }
-})
+}
+
+//will check for matches
+function checkForMatch(){
+  var cards= document.querySelectorAll('img')
+  const optionOne = cardsChosen[0]
+  const optionTwo = cardsChosen[1]
+
+  if (cardsChosen[0] === cardsChosen[1]){
+    //alert('You found a match!')
+    cards[optionOne].setAttribute('src','./assets/img/blank.png')
+    cards[optionTwo].setAttribute('src', './assets/img/blank.png')
+    cardsWon.push(cardsChosen)
+  } else{
+    //flip the card around to play again
+    cards[optionOne].setAttribute('src','./assets/img/random.png' )
+    cards[optionTwo].setAttribute('src','./assets/img/random.png' )
+    //alert('sorry, try again')
+  }
+    cardsChosen=[]
+    cardsChosenId= []
+    resultDisplay.textContent= cardsWon.length
+    //gives a point for every match
+    if (cardsWon.length === cardsArray.length/2){
+      //collected al cards in array
+      resultDisplay.textContent = 'Congratulations! You found all the matches'
+    }
+}
+//flips cards
+function flipCard(){
+var cardId= this.getAttribute('data-id')
+cardsChosen.push(cardArray[cardId].name)
+cardsChosenId.push(cardId)
+//add img to square
+this.setattribute('src', cardArray[cardId].img)
+
+if (cardsChosen.length ===2){
+  //so it doesnt happen too quickly
+  setTimeout(checkForMatch,500)
+}
+}
 createBoard()
