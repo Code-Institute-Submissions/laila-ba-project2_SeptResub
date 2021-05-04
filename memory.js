76a29338e4b2e3cc34ee2fdd560fe9c3ef87c84a
laila-ba <75024926+laila-ba@users.noreply.github.com>
2021-05-04 18:53:13 +0100

    Add files via upload
    added class

--- memory.js
+++ memory.js
@@ -67,8 +67,8 @@ var cardsWon = [];
 //loop over card array + create image elements
 function createGame(){
   for (let i = 0; i < cardList.length; i++){
-    var card = document.createElement('img');
-    card.setAttribute('src', './assets/img/random.png');
+    var card = document.createElement('game-cards');
+    card.setAttribute('class', 'game-cards' './assets/img/random.png');
     //give each card data id
     card.setAttribute('data-id', i);
     card.addEventListener('click',flipCard);
@@ -78,7 +78,7 @@ function createGame(){
 
 //will check for matches
 function checkForMatch(){
-  var cards= document.querySelectorAll('img');
+  var cards= document.querySelectorAll('game-cards');
   const optionOne = cardsChosenId[0];
   const optionTwo = cardsChosenId[1];
   if (cardsChosen[0] === cardsChosen[1]){
@@ -108,7 +108,7 @@ var cardId= this.getAttribute('data-id');
 cardsChosen.push(cardList[cardId].name);
 cardsChosenId.push(cardId);
 //add img to square based on cardID
-this.setAttribute('src', cardList[cardId].img);
+this.setAttribute('src', cardList[cardId].'game-cards');
 if (cardsChosen.length === 2){
   //so it doesnt happen too quickly
   setTimeout(checkForMatch,500);
